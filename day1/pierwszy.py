2e570878d27a2a84c5c2a0780e0a11f20d53b3b5
rajkonkret <rajkonkret660@gmail.com>
2026-02-16 11:30:54 +0100

    [1 2] add pierwszy.py

---
+++ day1/pierwszy.py
@@ -0,0 +1,16 @@
+# snake_case - konwencja nazewniczna
+
+print('Hello World') # wypisz/wydrukuj
+print("Hello World")
+# blake8 - formater bardziej restrykcyjny
+
+# ctrl / - komentarz
+# print("Hello World!')
+#   File "C:\Users\CSComarch\PycharmProjects\PythonProjectPodstawy-16-02-2026\day1\pierwszy.py", line 6
+#     print("Hello World!')
+#           ^
+# SyntaxError: unterminated string literal (detected at line 6)
+print("Hello World!!!")
+# Hello World
+# Hello World
+# Hello World!!!
