e54634e3ef692351bb0394175b3f7ff5f4056120
rajkonkret <rajkonkret660@gmail.com>
2026-02-16 13:00:19 +0100

    [1 15] update formaty.py

--- day1/formaty.py
+++ day1/formaty.py
@@ -5,7 +5,7 @@ wersja = 3.900001
 print(type(wersja))  # <class 'float'>, liczby zmiennoprzecinkowe
 liczba = 908789876564321123  # int
 
-print("Witaj %s, masz teraz %d lat." % (user, wiek)) # Witaj Tomek, masz teraz 39 lat.
+print("Witaj %s, masz teraz %d lat." % (user, wiek))  # Witaj Tomek, masz teraz 39 lat.
 # %s - string
 # %d - digit - liczba
 
@@ -15,3 +15,22 @@ print("Witaj %s, masz teraz %d lat." % (user, wiek)) # Witaj Tomek, masz teraz 3
 
 # Witaj Tomek, masz teraz 39 lat. fstringiem
 print(f"Witaj {user}, masz teraz {wiek} lat.")
+
+# %i - int
+# %f - float
+
+print("używamy wersji Pythona %i" % 3)  # używamy wersji Pythona 3
+print("używamy wersji Pythona %f" % 3)  # używamy wersji Pythona 3.000000
+print("używamy wersji Pythona %.2f" % 3.9)  # używamy wersji Pythona 3.90
+print("używamy wersji Pythona %.1f" % 3.9)  # używamy wersji Pythona 3.90
+print("używamy wersji Pythona %.0f" % 3.9)  # używamy wersji Pythona 4 - zaokrągla
+print("używamy wersji Pythona %.f" % 3.9)  # używamy wersji Pythona 4 - zaokrągla
+
+x = 3.8769
+print(x)
+y = round(x)
+print(y)  # 4
+
+z = round(x, 2)
+print(f'{z=}')  # z=3.88
+print(type(z))  # <class 'float'>
