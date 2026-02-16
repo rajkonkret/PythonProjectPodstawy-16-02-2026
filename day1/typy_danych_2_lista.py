d1570c5fe23cb74595bb836d5d048725d7ae5cfb
rajkonkret <rajkonkret660@gmail.com>
2026-02-16 15:03:17 +0100

    [1 23] update typy_danych_2_lista.py

--- day1/typy_danych_2_lista.py
+++ day1/typy_danych_2_lista.py
@@ -62,3 +62,10 @@ print(lista[:])  # ['Radek', 'Tomek', 'Marek', 'Zenek', 'Anna', 'Magda']
 #      -6       -5       -4       -3      -2      -1
 print(lista[-2:0])  # [] -> [4:0]
 print(lista[0:-2])  # ['Radek', 'Tomek', 'Marek', 'Zenek'] -> [0:4]
+
+# range() - generowanie liczb z zakresu (int)
+lista_15 = list(range(15))
+print(lista_15)  # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+
+print(lista_15[::2])  # [start:stop:krok], [0, 2, 4, 6, 8, 10, 12, 14]
+print(lista_15[::3])  # [0, 3, 6, 9, 12]
