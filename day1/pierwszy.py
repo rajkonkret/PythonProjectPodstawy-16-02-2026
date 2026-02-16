e1cc216070a73f47fbc4b16a3e97b41ba89f13b4
rajkonkret <rajkonkret660@gmail.com>
2026-02-16 11:48:32 +0100

    [1 3] update pierwszy.py

--- day1/pierwszy.py
+++ day1/pierwszy.py
@@ -1,6 +1,7 @@
 # snake_case - konwencja nazewniczna
+import sys
 
-print('Hello World') # wypisz/wydrukuj
+print('Hello World')  # wypisz/wydrukuj
 print("Hello World")
 # blake8 - formater bardziej restrykcyjny
 
@@ -14,3 +15,27 @@ print("Hello World!!!")
 # Hello World
 # Hello World
 # Hello World!!!
+
+print('"Radek"')  # "Radek"
+print('Radek \"Radek\"')  # Radek "Radek"
+
+# type() - sprawdzenie typu danych
+print(type("Radek"))  # <class 'str'>, dane tekstowe
+
+print('39' + '30')  # 3930, konkatenacja, łączenie stringów
+
+print(39 + 30)  # 69
+print(type(39))  # <class 'int'>, integer, liczby całkowite
+
+# musimy upewnic sie, ze mamy import sys
+print(sys.int_info)
+# sys.int_info(bits_per_digit=30, sizeof_digit=4,
+# default_max_str_digits=4300, str_digits_check_threshold=640)
+
+# print("39" + 39) # TypeError: can only concatenate str (not "int") to str
+
+# rzutowanie typów, zamiana
+print(int("39"))  # rzutowanie na liczbę cąlkowita, int
+print(39 + int("39"))  # 78
+
+print("39" + str(39))  # 3939
