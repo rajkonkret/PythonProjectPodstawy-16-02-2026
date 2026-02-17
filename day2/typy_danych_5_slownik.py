ad1c8f3f50f58b0f3623d50e54dc56b94c3ae38b
rajkonkret <rajkonkret660@gmail.com>
2026-02-17 10:39:04 +0100

    [2 12] update typy_danych_5_slownik.py

---
+++ day2/typy_danych_5_slownik.py
@@ -0,0 +1,15 @@
+# słownik - para klucz:wartosc
+# {"user" : "Radek"}
+# odpowiednik jsona
+# klucze nie mogą sie powtarzać
+
+# pusty słownik
+dictionary = {}
+print(dictionary)  # {} - pusty słownik
+print(type(dictionary))  # <class 'dict'>
+
+dictionary_1 = dict()
+print(dictionary_1)  # {}
+print(type(dictionary_1))  # <class 'dict'>
+
+
