3c9f51ff55c9294e9ef6ee35c88eb083ed22a2f8
rajkonkret <rajkonkret660@gmail.com>
2026-02-17 12:03:37 +0100

    [2 21] add warunki.py

---
+++ day2/warunki.py
@@ -0,0 +1,26 @@
+# warunki
+# instrukcje warunkowe
+# instrukcje sterowania przpływem programu
+# if
+# w zależności od warunku wykona jeden lub drugi blok programu
+# wyrażenie w warunku musi zwrócić typ bool
+
+odp = True
+
+if odp: print("Test")  # Test
+
+if odp:
+    print("Test")  # Test
+
+if odp:  # blok programu wykonywany gdy warunek True
+    print("Brawo")
+    # print("Brawo") IndentationError: unexpected indent
+    print("Brawo")
+    print("Brawo")
+    print("Brawo")
+    print("Brawo")
+# Brawo
+# Brawo
+# Brawo
+# Brawo
+# Brawo
