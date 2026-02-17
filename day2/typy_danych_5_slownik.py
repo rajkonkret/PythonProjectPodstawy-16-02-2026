3ec0a411935e04023e3b3e56c31712a0d4171af2
rajkonkret <rajkonkret660@gmail.com>
2026-02-17 10:52:25 +0100

    [2 14] update typy_danych_5_slownik.py

--- day2/typy_danych_5_slownik.py
+++ day2/typy_danych_5_slownik.py
@@ -12,4 +12,28 @@ dictionary_1 = dict()
 print(dictionary_1)  # {}
 print(type(dictionary_1))  # <class 'dict'>
 
+# dodanie lementów do słownika
+dictionary['imie'] = "Radek"
+print(dictionary)  # {'imie': 'Radek'}
 
+# dodać klucz wiek
+dictionary['wiek'] = 50
+print(dictionary)  # {'imie': 'Radek', 'wiek': 50}
+
+print(dictionary.keys())  # dict_keys(['imie', 'wiek'])
+print(dictionary.values())  # dict_keys(['imie', 'wiek'])
+print(dictionary.items())  # dict_items([('imie', 'Radek'), ('wiek', 50)])
+
+# nadpisanie wartości
+dictionary['imie'] = "Tomek"
+print(dictionary)  # {'imie': 'Tomek', 'wiek': 50}
+
+# wypisanie wartości dla klucza
+print(dictionary['imie'])  # Tomek
+
+dictionary['imie'] = ["Radek", "Tomek", "Magda"]
+print(dictionary)  # {'imie': ['Radek', 'Tomek', 'Magda'], 'wiek': 50}
+
+# wypisac Tomka
+print(dictionary['imie'])  # ['Radek', 'Tomek', 'Magda']
+print(dictionary['imie'][1])  # Tomek
