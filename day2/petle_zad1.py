4d5860f566c00ccfa48f621cd7b7642ad35845ee
rajkonkret <rajkonkret660@gmail.com>
2026-02-17 14:57:04 +0100

    [2 34] update petle_zad1.py

--- day2/petle_zad1.py
+++ day2/petle_zad1.py
@@ -7,4 +7,16 @@ for i in range(5):  # 0 do 4
 for i in range(20):
     pass  # nic nie rób
 
+print(i)  # 19
 
+for _ in range(15):  # niema zmienna
+    print("Test podłoga - niema zmienna")
+    print(_)  # można odczytać wartość
+# Test podłoga - niema zmienna
+# 13
+# Test podłoga - niema zmienna
+# 14
+
+for i in range(5):
+    print(i + 2)
+    print(i * 2)
