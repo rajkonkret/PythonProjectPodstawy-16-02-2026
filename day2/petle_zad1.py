730eb61760aab1f0e63204f71cc4af221cd2c3ab
rajkonkret <rajkonkret660@gmail.com>
2026-02-17 15:18:56 +0100

    [2 35] update petle_zad1.py

--- day2/petle_zad1.py
+++ day2/petle_zad1.py
@@ -1,5 +1,6 @@
 # pętla - możliwość wykonania kodu wielokrotnie
 # for - pętla iteracyjna
+import random
 
 for i in range(5):  # 0 do 4
     print(i)
@@ -20,3 +21,16 @@ for _ in range(15):  # niema zmienna
 for i in range(5):
     print(i + 2)
     print(i * 2)
+
+# przerobic lotto na pętle
+# zapisac wyniki do nowej listy
+
+lista_wynik = []
+lista_kul = list(range(1, 50))  # od 1 do 49
+for _ in range(6):  # od 0 do 5
+    kula = random.choice(lista_kul)
+    lista_kul.remove(kula)
+    # print(kula)  # 6
+    lista_wynik.append(kula)
+
+print(lista_wynik)  # [13, 23, 3, 42, 15, 7]
