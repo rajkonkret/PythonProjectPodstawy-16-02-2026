3422be3942644248bb8abe380eb6ddcdfd07057f
rajkonkret <rajkonkret660@gmail.com>
2026-02-18 12:39:18 +0100

    [3 18] update json_zad2.py

--- day3/json_zad2.py
+++ day3/json_zad2.py
@@ -6,7 +6,7 @@
 #         "term": "Co to jest \u201ename mangling\u201d w Pythonie?"
 #     }
 
-resp = '''# {
+resp = '''{
          "description": "\u201eName mangling\u201d to mechanizm zmiany nazwy atrybut\u00f3w klasy, kt\u00f3re s\u0105 zdefiniowane jako prywatne, co ma na celu unikni\u0119cie konflikt\u00f3w nazw w klasach pochodnych.",
          "example": "print(\"Przyk\u0142ad do: Co to jest \u201ename mangling\u201d w Pythonie?\")",
          "id": 30,
