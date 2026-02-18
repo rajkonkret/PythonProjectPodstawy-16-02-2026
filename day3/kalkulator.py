0fe02a4505a38d13efd7f55a2fdbef457599c52b
rajkonkret <rajkonkret660@gmail.com>
2026-02-18 10:47:28 +0100

    [2 48] update petle_zad3.py

--- day3/kalkulator.py
+++ day3/kalkulator.py
@@ -1,5 +1,40 @@
 # napiac aplikację kalkulator
-# while
+# while True
 # menu z opcjami
 # wyświetlic ładnie wynik
 # obsłużyć wyjątki
+
+while True:
+    print("""
+    1. Dodawanie
+    2. Odejmowanie
+    3. Mnożenie
+    4. Dzielenie
+    5. Koniec""")
+
+    odp = input("Podaj wybraną opcję:")
+
+    # if odp == "5":
+    #     break
+
+    if odp not in ["1", "2", "3", "4"]:
+        break
+
+    try:
+        a = int(input("Podaj pierwszą liczbę:"))
+        b = int(input("Podaj drugą liczbę:"))
+
+        if odp == "1":
+            print(f'Wynik {a} + {b} = {a + b}')
+        elif odp == "2":
+            print(f'Wynik {a} - {b} = {a - b}')
+        elif odp == "3":
+            print(f'Wynik {a} * {b} = {a * b}')
+        elif odp == "4":
+            print(f'Wynik {a} / {b} = {a / b}')
+    except ZeroDivisionError:
+        print("Nie dziel prze zero!!!")
+    except Exception as e:
+        print("Bład:", e)
+    finally:
+        print("Obliczenia wykonane")
