4068943d57f3a7ee4059e785bd8662761c291c69
rajkonkret <rajkonkret660@gmail.com>
2026-02-18 15:54:49 +0100

    [3 42] update fun5_kantor.py.py

--- day3/fun5_kantor.py
+++ day3/fun5_kantor.py
@@ -2,3 +2,15 @@
 # ma mieć dwie funkcje wewnętrzne: eur, usd -> 2 x def
 # w zależności od parametru zwroci jedną z funkcji -> if -> return
 # przekazenie kwoty do funkcji usd i eur
+
+def kantor(waluta):
+    def usd():
+        pass
+
+    def eur():
+        pass
+
+    if waluta == "eur":
+        pass
+    else:
+        pass
