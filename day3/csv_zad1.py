1ce3fed870aa9e040ffcbd7be0e95080b91f998a
rajkonkret <rajkonkret660@gmail.com>
2026-02-18 13:27:30 +0100

    [3 22] update csv_zad1.py

--- day3/csv_zad1.py
+++ day3/csv_zad1.py
@@ -7,9 +7,11 @@
 import csv
 
 row = ['radek', "coe", "3", 0]
+fields = ['name', 'branch', 'year', 'cgpa']
 
 filename = "records.csv"
 
 with open(filename, "w") as csv_f:
     csvwriter = csv.writer(csv_f)
+    csvwriter.writerow(fields)
     csvwriter.writerow(row)
