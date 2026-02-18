08473f092a7ae22a9040706e02dc351d10ba1e1d
rajkonkret <rajkonkret660@gmail.com>
2026-02-18 15:15:09 +0100

    [3 36] update fun2.py

---
+++ day3/fun2.py
@@ -0,0 +1,33 @@
+# funkcje zwracające wynik
+# kończy się : return
+a = 9
+b = 89
+
+
+def dodaj():
+    return a + b
+
+
+def dodaj2(a, b):
+    return a + b
+
+
+def odejmij(a=0, b=0, c=0):
+    return a - b - c
+
+
+print(dodaj())  # 98
+
+# dodaj2, odejmij
+print(dodaj2(6, 90))  # 96
+print(dodaj2(b=6, a=90))  # 96
+
+print(odejmij())  # 0
+print(odejmij(1))  # 1
+print(odejmij(1, 2))  # -1
+print(odejmij(1, 2, 3))  # -4
+print(odejmij(c=1, a=2, b=3))  # -2
+print(odejmij(c=1, a=2))  # 1
+
+wynik = odejmij(6, 8, 24)
+print("Wynik:", wynik)  # Wynik: -26
