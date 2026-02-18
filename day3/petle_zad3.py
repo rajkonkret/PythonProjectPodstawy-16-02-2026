f7a92ac98ad7e33bef19972fcfbbdcba0078ab0f
rajkonkret <rajkonkret660@gmail.com>
2026-02-18 09:53:21 +0100

    [2 42] update petle_zad2.py

---
+++ day3/petle_zad3.py
@@ -0,0 +1,12 @@
+# while - pętla sterowana warunkiem
+
+# pętla nieskończona
+# while True:
+#     print('Komunikat')
+
+licznik = 0
+while True:
+    licznik += 1  # licznik = licznik + 1
+    print("Komunikat 2 !!")
+    if licznik > 10:
+        break  # przerwanie pętli
