cc10fa2eccde1af7832e72504688a678556e7302
rajkonkret <rajkonkret660@gmail.com>
2026-02-18 10:15:50 +0100

    [2 46] update petle_zad3.py

--- day3/petle_zad3.py
+++ day3/petle_zad3.py
@@ -29,17 +29,28 @@ while licznik < 10:
 # Podaj ponownie:SECRET
 # Podaj ponownie:secret
 
-lista = []
-lista_int = []
-while True:
-    #  A string is numeric if all characters in the string are numeric
-    wej = input("Podaj liczbę:")  # str
-    if not wej.isnumeric():
-        break
-    lista.append(wej)
-    lista_int.append(int(wej))
-
-print(lista)  # ['1', '2', '3', '4', '5', '6', '7']
-print(lista_int)  # [1, 2, 3, 4, 5, 6, 7]
+# lista = []
+# lista_int = []
+# while True:
+#     #  A string is numeric if all characters in the string are numeric
+#     wej = input("Podaj liczbę:")  # str
+#     if not wej.isnumeric():
+#         break
+#     lista.append(wej)
+#     lista_int.append(int(wej))
+#
+# print(lista)  # ['1', '2', '3', '4', '5', '6', '7']
+# print(lista_int)  # [1, 2, 3, 4, 5, 6, 7]
 
 my_list = [1, 5, 2, 3, 5, 4, 5, 6, 5]
+while 5 in my_list:
+    my_list.remove(5)
+
+print(my_list)  # [1, 2, 3, 4, 6] # usunięte duplikaty, zachowana kolejność
+
+my_list = [1, 5, 2, 3, 5, 4, 5, 6, 5]
+print(dict.fromkeys(my_list))
+# {1: None, 5: None, 2: None, 3: None, 4: None, 6: None}
+
+print(list(dict.fromkeys(my_list)))
+# [1, 5, 2, 3, 4, 6] - bez utraty kolejnosci, zwróciło nową listę
