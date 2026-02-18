fbce7e37f572ecd0f3148607a4489d9b147464c3
rajkonkret <rajkonkret660@gmail.com>
2026-02-18 14:39:16 +0100

    [3 33] update fun1.py

--- day3/fun1.py
+++ day3/fun1.py
@@ -15,8 +15,16 @@ def dodaj2(a, b):  # argumenty obowiązkowe
     print(a + b)
 
 
+# ominięcie problemu przeciążania funkcji liczbą argumentów
+def odejmij(a, b, c=0):  # c=0 argument o wartości domyślnej
+    print(a - b - c)
+
+
 # wywołanie funkcji
 dodaj()  # 14
 
 # dodaj2()  # TypeError: dodaj2() missing 2 required positional arguments: 'a' and 'b'
 dodaj2(6, 90)  # 96
+
+odejmij(1, 2)  # -1
+odejmij(1, 2, 3)  # -4
