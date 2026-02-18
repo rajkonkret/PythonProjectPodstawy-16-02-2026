4ca9d6daa89cb10ebdb9be0bbf3795d5a50adc5f
rajkonkret <rajkonkret660@gmail.com>
2026-02-18 12:43:10 +0100

    [3 19] update json_zad2.py

--- day3/json_zad2.py
+++ day3/json_zad2.py
@@ -5,11 +5,19 @@
 #         "level": "podstawowy",
 #         "term": "Co to jest \u201ename mangling\u201d w Pythonie?"
 #     }
+import json
 
-resp = '''{
+# r - raw tekst - znaki specjalne traktuje jako zwykły tekst
+resp = r'''{
          "description": "\u201eName mangling\u201d to mechanizm zmiany nazwy atrybut\u00f3w klasy, kt\u00f3re s\u0105 zdefiniowane jako prywatne, co ma na celu unikni\u0119cie konflikt\u00f3w nazw w klasach pochodnych.",
          "example": "print(\"Przyk\u0142ad do: Co to jest \u201ename mangling\u201d w Pythonie?\")",
          "id": 30,
          "level": "podstawowy",
          "term": "Co to jest \u201ename mangling\u201d w Pythonie?"
      }'''
+
+dane = json.loads(resp)
+print(dane)
+print(type(dane))  # <class 'dict'>
+
+# wypisac wszystkie klucze ze słownika
