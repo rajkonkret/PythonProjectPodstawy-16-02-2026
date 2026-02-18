e1f9f8ce85961b02a4597ca13a0fc88d6885d53c
rajkonkret <rajkonkret660@gmail.com>
2026-02-18 11:24:32 +0100

    [3 1] update discount.py

--- day3/discount.py
+++ day3/discount.py
@@ -13,3 +13,24 @@ print(today.year)  # 2026
 formated_date = datetime.now().strftime("%d/%m/%Y")
 print("Dzisiejsza data:", formated_date)  # Dzisiejsza data: 18/02/2026
 print(type(formated_date))  # <class 'str'>
+
+# 11:16 11:16:56
+# 11:16 am
+formated_time = datetime.now().strftime("%H:%M:%S")
+print("Aktualna godzina:", formated_time)  # Aktualna godzina: 11:21:06
+print(type(formated_time))  # <class 'str'>
+
+# Aktualna godzina: 11:20:14
+# <class 'str'>
+
+formated_time_12h = datetime.now().strftime("%I:%M:%S %p")
+print("Aktualna godzina (12h):", formated_time_12h)  # Aktualna godzina (12h): 11:21:06 AM
+print(type(formated_time_12h))  # <class 'str'>
+# Aktualna godzina (12h): 11:21:06 AM
+# <class 'str'>
+
+object_data = datetime.now().strptime("18/02/2026", "%d/%m/%Y")
+print(object_data)
+print(type(object_data))
+# 2026-02-18 00:00:00
+# <class 'datetime.datetime'>
