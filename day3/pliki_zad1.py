c0c2dd850b89793a15b692a3baeab498440ef825
rajkonkret <rajkonkret660@gmail.com>
2026-02-18 11:56:34 +0100

    [3 7] update pliki_zad1.py.py

--- day3/pliki_zad1.py
+++ day3/pliki_zad1.py
@@ -4,4 +4,22 @@
 
 # filehandler
 
-open("test.log", "")
+# ========= ===============================================================
+#     Character Meaning
+#     --------- ---------------------------------------------------------------
+#     'r'       open for reading (default)
+#     'w'       open for writing, truncating the file first
+#     'x'       create a new file and open it for writing
+#     'a'       open for writing, appending to the end of the file if it exists
+#     'b'       binary mode
+#     't'       text mode (default)
+#     '+'       open a disk file for updating (reading and writing)
+#     ========= ===============================================================
+
+# w - tworzy nowy plik
+# gdy plik istnieje zostanie nadpisany (skasowany) !!!
+with open("test.log", "w") as f:
+    f.write('Powitanie\n')
+    f.write('Jeszcze jedno\n')
+
+# f.write("") # ValueError: I/O operation on closed file. plik został zamknięty przez context manager
