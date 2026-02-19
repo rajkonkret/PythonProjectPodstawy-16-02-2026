d6df26c9bc3aa8226c423814d66e7374072d0477
radoslawjaniak <rajkokret660@gmail.com>
2026-02-19 14:37:51 +0100

    [4 34] update kl4

--- day4/kl4.py
+++ day4/kl4.py
@@ -1,4 +1,8 @@
-class Ptak:
+from abc import ABC, abstractmethod
+
+
+# klasa abstrakcyjna - kalsa, która zawiera metode abstrakcyjną
+class Ptak(ABC):
     """
     Klasa opisująca ptaka w Pythonie
     """
@@ -16,6 +20,8 @@ class Ptak:
     def latam(self):
         print("Tu", self.gatunek, "Lecę z szybkością:", self.szybkosc, "km/h")
 
+    # metoda abstrakcyjna
+    @abstractmethod
     def wydaj_odglos(self):
         pass
 
@@ -46,15 +52,49 @@ class Orzel(Ptak):
         print("Kier kir kier ki kir")
 
 
-or1 = Ptak("Orzeł", 50)
-print(or1)  # <__main__.Ptak object at 0x105ffa270>
-or1.latam()  # Tu Orzeł Lecę z szybkością: 50 km/h
+class Sowa(Ptak):
+    """
+    Klasa Sowa dziedziczy po klasie Ptak
+    """
 
-kur1 = Ptak("Kura", 0)
-kur1.latam()  # Tu Kura Lecę z szybkością: 0 km/h
+
+# TypeError: Can't instantiate abstract class Ptak without an implementation for abstract method 'wydaj_odglos'
+# klasa abstrakcyjna
+# nie można tworzyć obiektów tej klasy
+# or1 = Ptak("Orzeł", 50)
+# print(or1)  # <__main__.Ptak object at 0x105ffa270>
+# or1.latam()  # Tu Orzeł Lecę z szybkością: 50 km/h
+# or1.wydaj_odglos()
+#
+# kur1 = Ptak("Kura", 0)
+# kur1.latam()  # Tu Kura Lecę z szybkością: 0 km/h
+# kur1.wydaj_odglos()
 
 kur2 = Kura("Kura zielononóżka")
 kur2.latam()  # Tu Kura zielononóżka Ja nie latam
+kur2.wydaj_odglos()
 
 or2 = Orzel("Orzeł Bielik", 55)
 or2.latam()  # Tu Orzeł Bielik Lecę z szybkością: 55 km/h
+or2.wydaj_odglos()
+
+# Tu Kura zielononóżka Ja nie latam
+# ko ko ko ko ko
+# Tu Orzeł Bielik Lecę z szybkością: 55 km/h
+# Kier kir kier ki kir
+
+# brak metody 'wydaj_odglos'w kalsie Sowa - nie można tworzyc obiektu klasy Sowe
+# TypeError: Can't instantiate abstract class Sowa without an implementation for abstract method 'wydaj_odglos'
+# sowa = Sowa("Sowa", 20)
+
+# polimorfizm - obiekty różnych klas, mogą byctraktowane jak obiekty wspolnej nadrzędnej klasy
+# wiąże je metoda  abstrakcyjna
+lista = [or2, kur2]  # obiekty róznych klas
+
+for i in lista:
+    i.wydaj_odglos()
+    print(i.__class__.__name__)
+# Kier kir kier ki kir
+# Orzel
+# ko ko ko ko ko
+# Kura
