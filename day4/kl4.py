b5ead15c21bafd68c3a7573ff45cd0163cf06263
radoslawjaniak <rajkokret660@gmail.com>
2026-02-19 14:15:03 +0100

    [4 30] update kl4

--- day4/kl4.py
+++ day4/kl4.py
@@ -17,5 +17,22 @@ class Ptak:
         print("Tu", self.gatunek, "Lecę z szybkością:", self.szybkosc, "km/h")
 
 
+class Kura(Ptak):
+    """
+    Klasa Kura, dziedziczy po klasie Ptak
+    """
+
+    # zmieniamy konstruktor w klasie dziedziczącej
+    def __init__(self, gatunek):
+        super().__init__(gatunek, 0)  # musimy wywołać super()
+
+    def latam(self):
+        print("Tu", self.gatunek, "Ja nie latam")
+
+
 or1 = Ptak("Orzeł", 50)
 print(or1)  # <__main__.Ptak object at 0x105ffa270>
+or1.latam()  # Tu Orzeł Lecę z szybkością: 50 km/h
+
+kur1 = Ptak("Kura", 0)
+kur1.latam()  # Tu Kura Lecę z szybkością: 0 km/h
