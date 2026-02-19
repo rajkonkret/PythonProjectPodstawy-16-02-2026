4c15fb4aed3f0287c08caa6a0d3044c80684a77e
radoslawjaniak <rajkokret660@gmail.com>
2026-02-19 12:12:11 +0100

    [4 19] update pakiet.py

---
+++ day4/kl1.py
@@ -0,0 +1,21 @@
+# klasa - przepis, szablon
+# cechy (zmienne)
+# metody - funkcja w klasie
+# obiekt - instancja klasy
+# klasa musi zostac najpierw zadeklarowna
+# tworzennie obiektu uruchamia funkcję inicjalizującą (konstroktur) __init__
+# paradygmaty -> hermetyzacja, dziedziczenie, polimorfizm, abstrakcja
+
+# PascalCase, UpperCamelCase
+class Human:
+    """
+    Klasa Human opisująca człowieka w Pythonie
+    """
+
+# tworzenie obiektu
+cz1 = Human()
+
+print(Human.__doc__)
+# Klasa Human opisująca człowieka w Pythonie
+
+print(print.__doc__)
