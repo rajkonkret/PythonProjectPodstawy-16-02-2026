72e5b62aabc83f5fdc227cc13f3dc913552d666c
radoslawjaniak <rajkokret660@gmail.com>
2026-02-19 15:27:46 +0100

    [4 38] update chuck_norris_api.py

--- day4/chuck_norris_api.py
+++ day4/chuck_norris_api.py
@@ -2,4 +2,24 @@
 # json, xml
 # GET, POST, PUT/PATCH, DELETE, HEAD
 
+# klient http
+import requests
+
+# pip install requests
+
 url = "https://api.chucknorris.io/jokes/random"
+
+response = requests.get(url)
+print(response)
+# <Response [200]>
+# https://pl.wikipedia.org/wiki/Kod_odpowiedzi_HTTP
+# 200 ok
+# 3xx - warningi, przekierowania
+# 4xx - 404 - brak strony, 400 Bad Request - błedne dane
+# 5xx - błedy po stronie serwera 500 Internal Server Error
+
+print(response.text) # json czyli tekst
+
+data = response.json()
+print(data)
+print(type(data))
