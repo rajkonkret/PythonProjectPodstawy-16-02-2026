599d6ac2617b9de88cd0f8cf3bd16ea0002ee0ff
radoslawjaniak <rajkokret660@gmail.com>
2026-02-19 09:59:22 +0100

    [4 6] update fun9

--- day4/fun9.py
+++ day4/fun9.py
@@ -2,3 +2,14 @@
 # skrócony zapis funkcji
 # lambda zwraca wynik (return)
 # funkcja anonimowa
+
+def odejmij(a, b):
+    return a - b
+
+
+print(odejmij(10, 8))  # 2
+
+odejmij2 = lambda a, b: a - b  # domyślnie ma return
+print(odejmij2(10, 2))  # 8
+
+# przemieni oblicz_vat na lambde
