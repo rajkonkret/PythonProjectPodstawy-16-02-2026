9091e11b9f2ed5d2c7b61c008b6cff77f0e4eee2
radoslawjaniak <rajkokret660@gmail.com>
2026-02-20 13:46:47 +0100

    [5 19] update wykres3.py

--- day5/wykres3.py
+++ day5/wykres3.py
@@ -4,4 +4,14 @@ xs = [0, 1, 2, 3, 4, 5]
 ys = [0, 2, 4, 2, 0, -2]
 zs = [0, 1, 0, 1, 0, 1]
 
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
 
+ax.plot_trisurf(xs, ys, zs, color='lightblue', edgecolor="gray")
+
+ax.set_title("Prosty wykres 3d - powierzchnia")
+ax.set_xlabel("X")
+ax.set_ylabel("Y")
+ax.set_zlabel("Z")
+
+plt.show()
