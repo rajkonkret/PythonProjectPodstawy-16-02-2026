fb69992721a66998f999d264ab1785b7d8428782
radoslawjaniak <rajkokret660@gmail.com>
2026-02-20 10:58:44 +0100

    [5 10] update baza3_orm.py.py

--- day5/baza3_orm.py
+++ day5/baza3_orm.py
@@ -4,4 +4,39 @@
 # sqlalchemy - orm w pythonie
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import declarative_base, sessionmaker
+
 # pip install sqlalchemy
+
+# konfiguracja silnika
+# echo=True - loguj zdarzenia bazy danych
+engine = create_engine("sqlite:///test.db", echo=True)
+
+# deklaracja klasy bazowej
+Base = declarative_base()
+
+
+# model, encja - kalsa odwzororwująca tabelę w bazie danych
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    age = Column(Integer)
+
+
+# tworzenie tabel w bazie danych
+Base.metadata.create_all(bind=engine)
+# CREATE TABLE users (
+# 	id INTEGER NOT NULL,
+# 	name VARCHAR,
+# 	age INTEGER,
+# 	PRIMARY KEY (id)
+# )
+
+# tworzenie sesji
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# dodawanie rekordu
+new_user = User(name="Jan", age=30)
+session.add(new_user)
+session.commit()
