0c27e2f80e550024b973eedb0faeba17f6e10ff2
radoslawjaniak <rajkokret660@gmail.com>
2026-02-20 14:09:46 +0100

    [5 23] update folium_zad1.py

---
+++ day5/zadanie_ai.py
@@ -0,0 +1,7 @@
+# https://colab.research.google.com/drive/14me1BSiJBslAW5rsozhSY7ylM5pHxMr_?authuser=0#scrollTo=70JM49s0KRMQ&uniqifier=1
+
+# można tylko wykonac na google colab
+from google.colab import ai
+
+response = ai.generate_text("What is the capital of Poland")
+print(response)
