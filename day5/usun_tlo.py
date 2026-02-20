f3e05907e5fe769490c08bd16c2690a666bdd5f5
radoslawjaniak <rajkokret660@gmail.com>
2026-02-20 14:22:25 +0100

    [5 24] update usun_tlo.py

---
+++ day5/usun_tlo.py
@@ -0,0 +1,22 @@
+from rembg import remove
+
+# pip install "rembg[cpu]"
+# pip install pillow
+
+# source .venv/bin/activate - aktywacja wirtualnego srodowiska
+
+input_path = 'input1.png'
+outpu_path = "output.png"
+
+with open(input_path, "rb") as f:
+    input_data = f.read()
+
+# print(input_data)
+
+output_data = remove(input_data)
+print(output_data)
+
+with open(outpu_path, 'wb') as f:
+    f.write(output_data)
+
+print("Tło usunięte")
